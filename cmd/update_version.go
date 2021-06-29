39a4f640d26a26e08f02cc63e8d0da0bad435f7b
Niki Driessen <niki.driessen@trendminer.com>
2021-06-29 14:13:13 +0200

    feat: support commit and pushing version updates

--- cmd/update_version.go
+++ cmd/update_version.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"gosh/git"
 	"gosh/log"
 )
 
@@ -20,10 +21,22 @@ var (
 				log.Fatal(err, "You must specify --stage or --release")
 			}
 			if appList, err := LoadAppList(flag, value); err == nil {
-				if err = appList.UpdateVersion(appName, version); err == nil {
-					log.Infof("Updated app %s to version %s for %s %s", appName, version, flag, value)
+				if repo, err := git.NewDeploymentRepository("", false); err == nil {
+					if err = appList.UpdateVersion(appName, version); err == nil {
+						push := GetBoolFlag(cmd, "push", false)
+						if push {
+							err = repo.Push(GetStringFlag(cmd, "message", ""))
+						}
+						if err == nil {
+							log.Infof("Updated app %s to version %s for %s %s", appName, version, flag, value)
+						} else {
+							log.Fatal(err, "Error pushing updates to deployment repository")
+						}
+					} else {
+						log.Fatal(err, "Error updating app %s to version %s for %s %s", appName, version, flag, value)
+					}
 				} else {
-					log.Fatal(err, "Error updating app %s to version %s for %s %s", appName, version, flag, value)
+					log.Fatal(err, "Error opening working dir as Git repository")
 				}
 			} else {
 				log.Fatal(err, "Error loading %s %s", flag, value)
@@ -35,5 +48,7 @@ var (
 func init() {
 	AddReleaseFlag(updateVersionCmd)
 	AddStageFlag(updateVersionCmd)
+	updateVersionCmd.Flags().BoolP("push", "p", false, "--push|-p   Push changes to the remote repository (default: false)")
+	updateVersionCmd.Flags().StringP("message", "m", "", "--message|-m \"COMMIT MESSAGE\" (optional, only used when --push is specified)")
 	updateCmd.AddCommand(updateVersionCmd)
 }
