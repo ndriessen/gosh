4ce072b9f1fdcb20bf51588374008658c0b908a3
Niki Driessen <niki.driessen@trendminer.com>
2021-07-01 10:26:57 +0200

    feat: add version command
    Add a version command to print the Gosh version

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"gosh/cmd"
+	"gosh/util"
 	"os"
 )
 
+var Version = "0.0.0"
+
 func main() {
+	util.Context.Version = Version
 	err := cmd.Execute()
 	if err != nil {
 		os.Exit(1)
--- util/context.go
+++ util/context.go
@@ -7,6 +7,7 @@ import (
 
 type GoshContext struct {
 	WorkingDir string
+	Version    string
 }
 
 var Context = &GoshContext{}
